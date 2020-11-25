4481e336ae3bd2be919baeb8eda7c903d824eb91
kaihulme <kaihulme97@gmail.com>
2020-11-25 19:13:16 +0000

    Centre polar coordinate origin for Hough transform

--- darts/transformation/houghlines.py
+++ darts/transformation/houghlines.py
@@ -10,40 +10,33 @@ class HoughLines():
         self.hough_space = []
         self.t_hough_space = []
 
-    def transform(self, frame):
+    def transform(self, frame, max_theta=180):
         """
         Apply Hough circles transformation to frame for a range of radii
         """
-        # create hough space
+        # create hough space with dimensions (-max ρ to max ρ, -max θ to max θ)
+        # where max ρ is the diagonal length of the input frame
+        # and max θ is (generally) 180°
         rows, cols = frame.shape
         max_rho = int(math.hypot(rows, cols))
-        max_theta = 180
         self.hough_space = np.zeros((max_rho*2, max_theta*2))
-
-        # precompute sin(theta), cos(theta) for all theta
+        # precompute sin(θ), cos(θ) for all θ
         thetas = np.arange(-max_theta, max_theta+1, self.t_step)
         cossin = np.column_stack((np.sin(thetas * math.pi / 180), 
                                   np.cos(thetas * math.pi / 180)))
-
-        # get positions of non-zero magnitude pixels and set inital radius
+        # get positions of non-zero magnitude pixels
         points = np.column_stack(np.nonzero(frame))
-
         for (y, x) in points:
-
-            # caluclate rho = ysin(theta) + xcos(theta)
+            # caluclate ρ = ysin(θ) + xcos(θ) for all θ
             rhos = y * cossin[:, 0] + x * cossin[:, 1]
-
-            # set line points as (rho, theta) and remove points not in space size
+            # set line points as each pair (ρ, θ) and remove points not in space size
             l_points = np.column_stack((rhos, thetas)).astype('int')
             l_points = l_points[np.where((l_points[:, 0] >= -max_rho) 
                                         & (l_points[:, 1] >= -max_theta) 
                                         & (l_points[:, 0] < max_rho) 
                                         & (l_points[:, 1] < max_theta))]
-
-            for (rho, theta) in l_points:
-                self.hough_space[rho+max_rho][theta+max_theta] += 1
-                # print(f"(theta, rho): ({theta}, {rho})")
-                # if (theta >= 0 and rho >= 0 and theta < rows and rho < cols):
+            # shift line points to centre polar origin and increment each in hough space
+            np.add.at(self.hough_space, (l_points[:, 0] + max_rho, l_points[:, 1] + max_theta, 1))
 
     def threshold(self, threshold_val):
         """
