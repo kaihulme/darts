49db49d37baa85f22505a9921df7f547dd183423
kaihulme <kaihulme97@gmail.com>
2020-12-09 16:58:35 +0000

    Fix IO dir

---
+++ darts/io/write.py
@@ -0,0 +1,52 @@
+import cv2 as cv
+import numpy as np
+from darts.io.read import getpath
+from darts.manipulation.utils import normalise, radtodeg
+
+def write(frame, name):
+    """
+    Normalise frame then write to /out with given name.
+    """
+    frame = normalise(frame)
+    path = getpath(name, "out")
+    cv.imwrite(path, frame)
+
+def gaussian(gaussian, name):
+    """
+    Write gaussian blurred frame to file.
+    """
+    write(gaussian, name + "_gaussian")
+
+def sobel(sobel, name):
+    """
+    Write sobel components to file.
+    """
+    write(sobel.dfdx, name + "_dfdx")
+    write(sobel.dfdy, name + "_dfdy")
+    write(sobel.magnitude, name + "_magnitude")
+    write(np.rad2deg(sobel.direction), name + "_direction")
+    write(sobel.t_magnitude, name + "_magnitude_threshold")
+
+def houghlines(houghlines, name):
+    """
+    Write hough lines to file. 
+    """
+    write(houghlines.hough_space, name + "_houghlines")
+    write(houghlines.t_hough_space, name + "_houghlines_threshold")
+
+def houghcircles(houghcircles, name, all=False):
+    """
+    Write hough spaces to file. 
+    """
+    if (all == True):
+        for s, (space, t_space) in enumerate(zip(houghcircles.hough_space, houghcircles.t_hough_space)):
+            write(space, name + "_houghcircles_" + str(s + 1))
+            write(t_space, name + "_houghcircles_" + str(s + 1) + "_threshold")
+    write(houghcircles.hough_space_sum, name + "_houghcircles_sum")
+    # write(houghcircles.t_hough_space_sum, name + "_houghcircles_sum_threshold")
+
+def circles(frame, name):
+    """
+    Write image with circles
+    """
+    write(frame, name + "_circles")
